18f7fda9c414d6f7d327cf9aa50a7d1efcfce96a
Ondeskon <118245966+Ondeskon@users.noreply.github.com>
2023-01-20 08:23:23 +0100

    Delete ukol os directory

--- ukol os/foo.c
+++
@@ -1,6 +0,0 @@
-#include "vystup.h"
-
-int foo (int x)
-{
-     return x + 5;
-}
--- ukol os/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "vystup.h"
-int main() {
-    printf("Hodnota %d", FOO_H); 
-   return 0;
-}       
