1994c0c2bbd6584ac69da187de6438a68b1d6eee
Ondeskon <118245966+Ondeskon@users.noreply.github.com>
2023-01-23 15:32:02 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-#include "vystup.h"
+
 int main() {
-    printf("Hodnota %d", FOO_H); 
-   return 0;
-}       
+    int number = 0;
+    int countOfChar;
+    do {
+        printf("Type a whole number:");
+        scanf("%d", &number);
+        /* Kontrola vstupně výstupního bufferu=pamět, do které se ukládají znaky z klávesnice.
+         * Funkce getchar() přečte 1 znak z vv bufferu a zárověn ho vymaže. Pokud vstupně výstupní buffer
+         * obsahuje znaky, funkce scanf nepozastaví konzoli a uživatel nemůže zadat další číslo.
+         * To způsobí nekonečný cyklus*/
+        countOfChar = 0;
+        while (getchar() != '\n') {
+            countOfChar++;
+        }
+        /* Pokud proměnná countOfChar se nerovná nule, ve vstupně výstupním bufferu byly nalezeny
+         * znaky, tj. uživatel nezadal celé číslo.*/
+        if (countOfChar!=0) {
+            printf ("No whole number has been typed. Try again.\n");
+        }
+        else if (number == 0) {
+            printf("The number %d is zero. Program will be closed.\n", number);
+        } else if (number > 0) {
+            printf("The number %d is positive.\n", number);
+        } else {
+            printf("The number %d is negative.\n", number);
+        }
+    } while (countOfChar !=0 || number != 0);
+    return 0;
+
+}
